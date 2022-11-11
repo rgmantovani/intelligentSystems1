4e445c92b5ba1e8f5832a80be1f58e25af5eab09
Rafael Gomes Mantovani <rgmantovani@gmail.com>
2022-11-11 01:07:19 -0300

    updating NB algo

--- codes/03_supervisedLearning/naiveBayes/naivebayes.r
+++ codes/03_supervisedLearning/naiveBayes/naivebayes.r
@@ -1,39 +1,44 @@
+# -------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------
 
-
-naive <- function(dataset, sample) {
+naive = function(dataset, sample) {
 
 	idClass = ncol(dataset)
+
+	#possiveis hipoteses (valores de classes)
 	possible_hyp = unique(dataset[,idClass])
 	answers = rep(0, length(possible_hyp))
 
-	# argmax
-	# para cada hipotese faca...
+	# argmax - para cada hipotese faca...
 	for (j in 1:length(possible_hyp)) {
 
-		# P(v_j) -> P(Não)
-		answers[j] = 
-		    sum(dataset[,idClass] == possible_hyp[j]) /
-			nrow(dataset)
+		# computar a probabilidade do label (classe h). Ex: P(v_j) -> P(Não)
+		answers[j] = sum(dataset[,idClass] == possible_hyp[j]) / nrow(dataset)
 
 		# dada aquela hipotese, verifique os atributos
-		rows = which(dataset[,idClass] == possible_hyp[j])
-		part = dataset[rows,]
-		prod = 1
+		# selectionando um subset de exemplos (subset), p fazer algumas contagens
+		rows   = which(dataset[,idClass] == possible_hyp[j])
+		subset = dataset[rows,]
 
+		# calcular o produtório
+		prod = 1
 		for (i in 1:length(sample)) {
 			# P(a_1 | Não) * ... * P(a_n | Não)
-			prod = prod * 
-				sum(sample[i] == part[,i]) / nrow(part)
+			prod = prod *sum(sample[i] == subset[,i]) / nrow(subset)
 		}
 
+		# computar a probabilidade condicional daquele valor de classe
 		answers[j] = answers[j] * prod
 	}
 
+	# normalizar os valores de probabilidade obtidos
 	answers = answers / sum(answers)
 
-	ret = list()
-	ret$possible_hyp = possible_hyp
-	ret$answers = answers
+	# retornar as contagens, mas sem escolher qual é a classe (fazer isso externamente)
+	ret = list(possible_hyp = possible_hyp, answers = answers)
+	return(ret)
 
-	ret
 }
+
+# -------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------
