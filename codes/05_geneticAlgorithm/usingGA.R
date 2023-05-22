f9084192553277ecaf3b493bb53fa67231e614b9
Rafael Gomes Mantovani <rgmantovani@gmail.com>
2023-05-22 11:18:50 -0300

    adding script using GA

---
+++ codes/05_geneticAlgorithm/usingGA.R
@@ -0,0 +1,31 @@
+# -------------------------------------------------------
+# -------------------------------------------------------
+
+set.seed(42)
+
+source("geneticAlgorithm.R")
+
+N = 100 # qtde de numeros que queremos separar
+problema = sample(x = -1000:1000, size = N)
+
+POP.SIZE = 100
+GERACOES = 1000
+
+teste = geneticAlgorithm(problem = problema, pop.size = POP.SIZE, 
+	generations = GERACOES)
+
+# -------------------------------------------------------
+# -------------------------------------------------------
+
+library(ggplot2)
+
+df = cbind(1:length(teste$average.fitness), teste$average.fitness)
+df = as.data.frame(df)
+colnames(df) = c("generation", "avgFitness")
+
+g = ggplot(df, aes(x = generation, y = avgFitness))
+g = g + geom_point() + geom_line()
+g
+
+# -------------------------------------------------------
+# -------------------------------------------------------
